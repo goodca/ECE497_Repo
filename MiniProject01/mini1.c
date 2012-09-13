5b54ea51aa5ea71ed4ed8707009356c5ac2252a1
goodca <goodca@rose-hulman.edu>
2012-09-13 10:10:20 +0000

    initial miniproject commit

---
+++ MiniProject01/mini1.c
@@ -0,0 +1,479 @@
+//Christopher A Good
+//20120915
+//mini project 1
+
+//some code from:
+/* Copyright (c) 2011, RidgeRun
+ * All rights reserved.
+ *
+From https://www.ridgerun.com/developer/wiki/index.php/Gpio-int-test.c
+
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *    This product includes software developed by the RidgeRun.
+ * 4. Neither the name of the RidgeRun nor the
+ *    names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY RIDGERUN ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL RIDGERUN BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+//i2c code based on:
+/*
+    Minimal version for teaching.  Mark A. Yoder 26-July-2011
+    i2cget.c - A user-space program to read an I2C register.
+    Copyright (C) 2005-2010  Jean Delvare <khali@linux-fr.org>
+
+    Based on i2cset.c:
+    Copyright (C) 2001-2003  Frodo Looijaard <frodol@dds.nl>, and
+                             Mark D. Studebaker <mdsxyz123@yahoo.com>
+    Copyright (C) 2004-2005  Jean Delvare <khali@linux-fr.org>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+    MA 02110-1301 USA.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <signal.h>
+#include "i2c-dev.h"
+
+ /****************************************************************
+ * Constants
+ ****************************************************************/
+ 
+#define SYSFS_GPIO_DIR "/sys/class/gpio"
+#define POLL_TIMEOUT (3 * 1000) /* 3 seconds */
+#define MAX_BUF 64
+#define pwmloc "/sys/class/pwm/ehrpwm.1:0"
+
+/****************************************************************
+ * Global variables
+ ****************************************************************/
+int keepgoing = 1;	// Set to 0 when ctrl-c is pressed
+
+/****************************************************************
+ * signal_handler
+ ****************************************************************/
+// Callback called when SIGINT is sent to the process (Ctrl-C)
+void signal_handler(int sig)
+{
+	printf( "Ctrl-C pressed, cleaning up and exiting..\n" );
+	keepgoing = 0;
+}
+
+/****************************************************************
+ * gpio_export
+ ****************************************************************/
+int gpio_export(unsigned int gpio)
+{
+	int fd, len;
+	char buf[MAX_BUF];
+ 
+	fd = open(SYSFS_GPIO_DIR "/export", O_WRONLY);
+	if (fd < 0) {
+		perror("gpio/export");
+		return fd;
+	}
+ 
+	len = snprintf(buf, sizeof(buf), "%d", gpio);
+	write(fd, buf, len);
+	close(fd);
+ 
+	return 0;
+}
+
+/****************************************************************
+ * gpio_unexport
+ ****************************************************************/
+int gpio_unexport(unsigned int gpio)
+{
+	int fd, len;
+	char buf[MAX_BUF];
+ 
+	fd = open(SYSFS_GPIO_DIR "/unexport", O_WRONLY);
+	if (fd < 0) {
+		perror("gpio/export");
+		return fd;
+	}
+ 
+	len = snprintf(buf, sizeof(buf), "%d", gpio);
+	write(fd, buf, len);
+	close(fd);
+	return 0;
+}
+
+/****************************************************************
+ * gpio_set_dir
+ ****************************************************************/
+int gpio_set_dir(unsigned int gpio, unsigned int out_flag)
+{
+	int fd, len;
+	char buf[MAX_BUF];
+ 
+	len = snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR  "/gpio%d/direction", gpio);
+ 
+	fd = open(buf, O_WRONLY);
+	if (fd < 0) {
+		perror("gpio/direction");
+		return fd;
+	}
+ 
+	if (out_flag)
+		write(fd, "out", 4);
+	else
+		write(fd, "in", 3);
+ 
+	close(fd);
+	return 0;
+}
+
+/****************************************************************
+ * gpio_set_value
+ ****************************************************************/
+int gpio_set_value(unsigned int gpio, unsigned int value)
+{
+	int fd, len;
+	char buf[MAX_BUF];
+ 
+	len = snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/value", gpio);
+ 
+	fd = open(buf, O_WRONLY);
+	if (fd < 0) {
+		perror("gpio/set-value");
+		return fd;
+	}
+ 
+	if (value)
+		write(fd, "1", 2);
+	else
+		write(fd, "0", 2);
+ 
+	close(fd);
+	return 0;
+}
+
+/****************************************************************
+ * gpio_get_value
+ ****************************************************************/
+int gpio_get_value(unsigned int gpio, unsigned int *value)
+{
+	int fd, len;
+	char buf[MAX_BUF];
+	char ch;
+
+	len = snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/value", gpio);
+ 
+	fd = open(buf, O_RDONLY);
+	if (fd < 0) {
+		perror("gpio/get-value");
+		return fd;
+	}
+ 
+	read(fd, &ch, 1);
+
+	if (ch != '0') {
+		*value = 1;
+	} else {
+		*value = 0;
+	}
+ 
+	close(fd);
+	return 0;
+}
+
+
+/****************************************************************
+ * gpio_set_edge
+ ****************************************************************/
+
+int gpio_set_edge(unsigned int gpio, char *edge)
+{
+	int fd, len;
+	char buf[MAX_BUF];
+
+	len = snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/edge", gpio);
+ 
+	fd = open(buf, O_WRONLY);
+	if (fd < 0) {
+		perror("gpio/set-edge");
+		return fd;
+	}
+ 
+	write(fd, edge, strlen(edge) + 1); 
+	close(fd);
+	return 0;
+}
+
+/****************************************************************
+ * gpio_fd_open
+ ****************************************************************/
+
+int gpio_fd_open(unsigned int gpio)
+{
+	int fd, len;
+	char buf[MAX_BUF];
+
+	len = snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR "/gpio%d/value", gpio);
+ 
+	fd = open(buf, O_RDONLY | O_NONBLOCK );
+	if (fd < 0) {
+		perror("gpio/fd_open");
+	}
+	return fd;
+}
+
+/****************************************************************
+ * gpio_fd_close
+ ****************************************************************/
+
+int gpio_fd_close(int fd)
+{
+	return close(fd);
+}
+
+/****************************************************************
+ * setpwm
+ ****************************************************************/
+
+int setpwm(int duty, int freq){
+//set up pwm
+	FILE *pwmfpfreq, *pwmfpduty, *fp;
+	//set the mux
+	//printf("about to set mux\n");
+	fflush(stdout);
+	fp = fopen("/sys/kernel/debug/omap_mux/gpmc_a2", "ab");
+	rewind(fp);
+	fprintf(fp, "6\n");	
+	fclose(fp);
+	//printf("about to open pwm files\n");
+	fflush(stdout);
+	//open files
+	pwmfpfreq = fopen(pwmloc "/period_freq", "ab");
+	rewind(pwmfpfreq);
+	
+	pwmfpduty = fopen(pwmloc "/duty_percent", "ab");
+	rewind(pwmfpduty);
+
+	fp = fopen(pwmloc "/run", "ab");
+	rewind(fp);
+	//turn on pwm and set values
+	fprintf(fp, "1\n");
+	rewind(fp);
+	fprintf(pwmfpduty, "0\n");
+	rewind(pwmfpduty);
+	fprintf(pwmfpfreq, "%d\n", freq);
+	rewind(pwmfpduty);
+	fprintf(pwmfpduty, "%d\n", duty);
+	rewind(pwmfpduty);
+	//close files
+	fclose(fp);
+	fclose(pwmfpfreq);
+	fclose(pwmfpduty);
+}
+
+int readAnalogIn(){
+	FILE *fp;
+	char buf[MAX_BUF];
+	//open file
+	fp = fopen("/sys/devices/platform/omap/tsc/ain3", "r");
+	if(fp == NULL){
+		printf("error with opening analog in file\n");
+		fflush(stdout);
+	}
+	rewind(fp);
+	//read value
+	fgets(buf, MAX_BUF, fp);
+	//printf("%s\n",buf);
+	int value = atoi(buf);
+	fclose(fp);
+	return value;
+	//return 0;
+	
+}
+
+int i2cget(){
+char *end;
+	int res, i2cbus, address, size, file;
+	int daddress;
+	char filename[20];
+
+
+	i2cbus   = 3;
+	address  = 0x4a;
+	daddress = 0;
+	size = I2C_SMBUS_BYTE;
+
+	sprintf(filename, "/dev/i2c-%d", i2cbus);
+	file = open(filename, O_RDWR);
+	if (file<0) {
+		if (errno == ENOENT) {
+			fprintf(stderr, "Error: Could not open file "
+				"/dev/i2c-%d: %s\n", i2cbus, strerror(ENOENT));
+		} else {
+			fprintf(stderr, "Error: Could not open file "
+				"`%s': %s\n", filename, strerror(errno));
+			if (errno == EACCES)
+				fprintf(stderr, "Run as root?\n");
+		}
+		exit(1);
+	}
+
+	if (ioctl(file, I2C_SLAVE, address) < 0) {
+		fprintf(stderr,
+			"Error: Could not set address to 0x%02x: %s\n",
+			address, strerror(errno));
+		return -errno;
+	}
+
+	res = i2c_smbus_read_byte_data(file, daddress);
+	close(file);
+
+	if (res < 0) {
+		fprintf(stderr, "Error: Read failed, res=%d\n", res);
+		exit(2);
+	}
+	//printf("%d\n", res);
+	//printf("0x%02x (%d)\n", res, res);
+	return res;
+}
+/****************************************************************
+ * Main
+ ****************************************************************/
+int main(int argc, char **argv, char **envp)
+{
+	struct pollfd fdset[2];
+	int nfds = 2;
+	int gpio_fd, timeout, rc, LEDgpio;
+	char *buf[MAX_BUF];
+	unsigned int gpio;
+	int len;
+	
+
+	/*if (argc < 2) {
+		printf("Usage: gpio-int <gpio-pin>\n\n");
+		printf("Waits for a change in the GPIO pin voltage level or input on stdin\n");
+		exit(-1);
+	}*/
+
+	// Set the signal callback for Ctrl-C
+	signal(SIGINT, signal_handler);
+	
+	gpio = 48;//atoi(argv[1]);
+	LEDgpio = 60;	
+		
+	gpio_export(LEDgpio);
+	gpio_set_dir(LEDgpio,1);
+	
+	//interrupt gpio
+	
+	gpio_export(gpio);
+	gpio_set_dir(gpio, 0);
+	gpio_set_edge(gpio, "both");  // Can be rising, falling or both
+	gpio_fd = gpio_fd_open(gpio);
+	
+	
+
+
+	timeout = POLL_TIMEOUT;
+ 	int i = 0;
+	while (keepgoing) {
+		memset((void*)fdset, 0, sizeof(fdset));
+
+		fdset[0].fd = STDIN_FILENO;
+		fdset[0].events = POLLIN;
+      
+		fdset[1].fd = gpio_fd;
+		fdset[1].events = POLLPRI;
+
+		rc = poll(fdset, nfds, timeout);      
+
+		if (rc < 0) {
+			printf("\npoll() failed!\n");
+			return -1;
+		}
+      
+		if (rc == 0) {
+			printf(".");
+		}
+            
+		if (fdset[1].revents & POLLPRI) {
+			lseek(fdset[1].fd, 0, SEEK_SET);  // Read from the start of the file
+			len = read(fdset[1].fd, buf, MAX_BUF);
+			printf("\npoll() GPIO %d interrupt occurred, value=%c, len=%d\n",
+				 gpio, buf[0], len);
+			char val = (char)buf[0];
+			
+			gpio_set_value(LEDgpio,(i2cget()>34));
+			setpwm(100*readAnalogIn()/4095,1000);
+			if(i < 99){
+				i++;
+			}
+			i2cget();
+			
+			//printf("%d\n",100*readAnalogIn()/4095);
+		}
+
+		if (fdset[0].revents & POLLIN) {
+			(void)read(fdset[0].fd, buf, 1);
+			printf("\npoll() stdin read 0x%2.2X\n", (unsigned int) buf[0]);
+		}
+
+		fflush(stdout);
+	}
+
+	gpio_fd_close(gpio_fd);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
