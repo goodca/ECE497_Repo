b26078181ea8fb44d4bfda21208b432d8a3b4b99
goodca <goodca@rose-hulman.edu>
2012-10-17 06:48:16 +0000

    SoftPot initial commit

---
+++ SoftPot/mini2.c
@@ -0,0 +1,30 @@
+/** demonstrates usage of the SoftPot variable potentiometer
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv, char **envp)
+{
+	FILE *fp;
+	char buf[64];
+	
+	//open file
+	fp = fopen("/sys/devices/platform/omap/tsc/ain3", "r");
+	if(fp == NULL){
+		printf("error with opening analog in file\n");
+		fflush(stdout);
+		exit(1);
+	}
+	while(1){ //will run forever
+		rewind(fp);
+		//read value
+		fgets(buf, 64, fp);
+		//format for printing
+		printf("The SoftPot was touched at %d%\n", 101*atoi(buf)/4095); //101 for 0-100
+		sleep(1); //will update once every second
+	}	
+	//close file pointer
+	fclose(fp);
+	return 0;
+}
